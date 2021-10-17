569ac3fa1aeb29d3f14ec6bb9b54adc6aca60d35
crigar <crigar888@gmail.com>
2021-10-18 00:49:59 +0200

    filter works fine

--- src/components/Filter.component.jsx
+++ src/components/Filter.component.jsx
@@ -2,38 +2,93 @@ import React, { Fragment, useState, useRef } from 'react';
 import { Movie } from './Movie.component';
 import { MoviePrev } from './MoviePrev.component';
 import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
 import MovieService from '../services/movie.service';
 import CategoryService from '../services/category.service';
 import Box from '@mui/material/Box';
 import Rating from '@mui/material/Rating';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
+import { createBrowserHistory } from 'history';
+import { useSearchParams } from 'react-router-dom'
 import {
     BrowserRouter as Router,
     Switch,
     Route,
-    useRouteMatch
+    useRouteMatch,
+    history
   } from "react-router-dom";
 
 
 export function Filter() {
-    let [movies, setMovies] = useState(MovieService.getMovies({}));
-    let [categories, setCategories] = useState(CategoryService.getCategories());
-    let [currentRating, setRating] = useState(); 
+    let history = createBrowserHistory();
+    let filterParams = new URLSearchParams(history.location.search);
+    console.log(filterParams.get('name'))
+    let [name, setName] = useState();
+    let [category, setCategory] = useState(filterParams.get('category'));
+    let [rating, setRating] = useState(parseInt(filterParams.get('rating'), 10));
+    let [year, setYear] = useState(filterParams.get('year')); 
     let years = MovieService.getYears();
+    let categories = CategoryService.getCategories();
     let match = useRouteMatch();
+    
+    
+    console.log(history.location)
+    if (filterParams.toString() !== '') {
+        history.replace({
+            pathname: '/',
+            search: "?" + filterParams.toString()
+        });
+        // setName(filterParams.get('name'));
+        // setCategory(filterParams.get('name'));
+        // setRating(filterParams.get('name'));
+        // setYear(filterParams.get('name'));
+    }
+    let [movies, setMovies] = useState(MovieService.getMovies(filterParams.toString()));
+    const setParams = () => {
+        history.replace({
+            pathname: '',
+            search: "?" + filterParams.toString()
+        });
+        setMovies(MovieService.getMovies(filterParams.toString()));
+    }
+    
+    const handleName = (event) => {
+        let value = event.target.value;
+        filterParams.set('name',value );
+        setName(value);
+        setParams();
+    }
+    const handleCategory = (event) => {
+        let value = event.target.lastChild.data;
+        filterParams.set('category', value);
+        setCategory(value);
+        setParams();
+    }
+    const handleRating = (event) => {
+        let value = parseInt(event.target.value);
+        filterParams.set('rating', value);
+        setRating(value);
+        setParams();
+    }
+    const handleYear = (event) => {
+        let value = event.target.lastChild?.data;
+        filterParams.set('year', value);
+        setYear(value);
+        setParams();
+    }
 
-    const handleFilter = (event) => {
-        const target = event.target;
-        const filter = {
-            name: target.value,
-            category: target.lastChild ? target.lastChild.data: undefined,
-            rating: parseInt(target.value, 10),
-            year: target.lastChild ? target.lastChild.data: undefined
-        };
-        console.log(target.value)
-        setRating(filter.rating);
-        setMovies(MovieService.getMovies(filter));
+    const handleClear = () => {
+        filterParams = new URLSearchParams('');
+        history.replace({
+            pathname: '/',
+            search: ''
+        });
+        setName('');
+        setCategory('');
+        setRating(0);
+        setYear('');
+        setMovies(MovieService.getMovies());
     }
     
     return (
@@ -44,7 +99,7 @@ export function Filter() {
                     </Grid>
                     <Grid item xs={12} md={3}>
                         <Box sx={{ minWidth: 120 }}>
-                        <TextField  label="Name" onChange={handleFilter} />
+                            <TextField  label="Name" onChange={handleName} value={name} />
                         </Box>
                     </Grid>
 
@@ -53,30 +108,36 @@ export function Filter() {
                             <Autocomplete
                                 disablePortal
                                 id="category-filter"
-                                onChange={handleFilter}
+                                onChange={handleCategory}
                                 options={categories}
                                 sx={{ width: 300 }}
+                                value={category}
                                 renderInput={(params) => <TextField {...params} label="Category" />}
                                 />
                         </Box>
                     </Grid>
 
-                    <Grid item xs={12} md={3}>
+                    <Grid item xs={12} md={2}>
                         <Box sx={{ minWidth: 120 }}>
-                            <Rating name="no-value" value={currentRating} onChange={handleFilter}/>
+                            <Rating name="no-value" value={rating} onChange={handleRating}/>
                         </Box>
                     </Grid>
 
-                    <Grid item xs={12} md={3}>
+                    <Grid item xs={12} md={2}>
                         <Box sx={{ minWidth: 120 }}>
                             <Autocomplete
                                 disablePortal
                                 id="year-filter"
                                 options={years}
-                                sx={{ width: 300 }}
-                                onChange={handleFilter}
-                                renderInput={(params) => <TextField {...params} label="Year" onChange={handleFilter} />}
+                                onChange={handleYear}
+                                value={year}
+                                renderInput={(params) => <TextField {...params} label="Year"  />}
                                 />
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} md={2}>
+                        <Box sx={{ minWidth: 120 }}>
+                            <Button variant="contained" onClick={handleClear}>Clear</Button>
                         </Box>
                     </Grid>
                                     
--- src/services/movie.service.js
+++ src/services/movie.service.js
@@ -1,22 +1,41 @@
 module.exports = {
     
-    getMovies(filter) {
-        console.log(filter)
-        console.log(Object.keys(filter))
-        let movies = this.movies.filter(
-            movie =>
-                movie.name.indexOf(filter.name) > -1 ||
-                filter.category === movie.category ||
-                filter.rating === movie.rating ||
-                filter.year === movie.year
-        );
-        console.log(movies)
+    getMovies(filterParams = undefined) {
+        let movies;
+        if (filterParams) {
+            filterParams = new URLSearchParams(filterParams);
+            
+            movies = this.movies.filter(
+                movie => {
+                    let name = movie.name.indexOf(filterParams.get('name')) > -1;
+                    if (filterParams.get('name') == null) {
+                        name = true;
+                    }
+                    let category = this.checkValueInMovie(filterParams.get('category'), movie.category);
+                    let rating = this.checkValueInMovie(filterParams.get('rating'), movie.rating);
+                    let year = this.checkValueInMovie(filterParams.get('year'), movie.year);
+                    return  name && rating && category && year;
+                }
+            )
+            console.log(movies)
+        } else {
+            movies = this.movies;
+        }
         return movies;
     },
 
     getYears() {
         return this.years
     },
+
+    checkValueInMovie(paramValue, movieAttribute){
+        if (paramValue && paramValue != 'undefined') {
+            return paramValue == movieAttribute           
+        } else {
+            return true;
+        }
+    },
+
     years: Array(new Date().getFullYear() + 1 - 1888).fill().map((_, i) => (i + 1888).toString()).reverse(),
     movies: [
         {
@@ -30,24 +49,24 @@ module.exports = {
         {
             id: 2,
             name: 'Movie2',
-            category: 'category',
-            rating: '5',
+            category: 'Comedia',
+            rating: 3,
             year: '2020',
             image: 'image',
         },
         {
             id: 3,
             name: 'Movie3',
-            category: 'category',
-            rating: '5',
-            year: '2020',
+            category: 'Aventuras',
+            rating: 3,
+            year: '2021',
             image: 'image',
         },
         {
             id: 4,
             name: 'Movie4',
             category: 'category',
-            rating: '5',
+            rating: 3,
             year: '2020',
             image: 'image',
         },
@@ -55,7 +74,7 @@ module.exports = {
             id: 5,
             name: 'Movie5',
             category: 'category',
-            rating: '5',
+            rating: 2,
             year: '2020',
             image: 'image',
         },
